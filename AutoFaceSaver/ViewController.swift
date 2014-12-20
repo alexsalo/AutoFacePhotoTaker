d1f8ef1a7f4a52d72bee618a6023d50d31099c95
Aleksandr Salo <saloasdev@gmail.com>
2014-12-19 21:18:24 -0600

    init - live preview works

--- AutoFaceSaver/ViewController.swift
+++ AutoFaceSaver/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import CoreMedia
+import AssetsLibrary
 
 class ViewController: UIViewController {
+    let captureSession = AVCaptureSession()
+    var captureDevice: AVCaptureDevice?
+    let stillImageOuput = AVCaptureStillImageOutput()
+    var assetCollection: PHAssetCollection!
+    var faceDetector: CIDetector?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var orientation: UIImageOrientation = .Up
 
+    @IBOutlet var viewLive: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        captureSession.sessionPreset = AVCaptureSessionPresetMedium
+        let devices = AVCaptureDevice.devices()
+        println(devices)
+        for device in devices{
+            if device.hasMediaType(AVMediaTypeVideo){
+                if device.position == AVCaptureDevicePosition.Front{
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+        if captureDevice != nil{
+            var err : NSError? = nil
+            captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+            if err != nil{
+                println("eroor: \(err?.localizedDescription)")
+            }
+            
+            if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession){
+                previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                var rootLayer = self.view.layer
+                rootLayer.masksToBounds = true
+                var frame = self.viewLive.frame
+                previewLayer.frame = frame
+                rootLayer.insertSublayer(previewLayer, atIndex: 0)
+                
+                var settings: NSDictionary = [AVVideoCodecKey: AVVideoCodecJPEG]
+                stillImageOuput.outputSettings = settings
+                captureSession.addOutput(stillImageOuput)
+                
+                captureSession.startRunning()
+                println("Session running")
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
